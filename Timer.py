db91eba380436b3153813efb9ed1ca8ad804d7f3
SurikovKirill <kirill98s@bk.ru>
2019-01-17 02:52:31 +0300

    notification

--- Timer.py
+++ Timer.py
@@ -1,15 +1,20 @@
 import sublime, sublime_plugin
 import threading  
 import time
-#import winsound
-#import subprocess
 
 i=0
-#def show_notification():
+def show_notification():
     #playsound.playsound('Sound_11330.wav', True)
     #winsound.PlaySound("Sound_11330.wav", winsound.SND_ASYNC)
-#sublime.message_dialog("Drink some water")
-#C:\Users\kiril\AppData\Roaming\Sublime Text 3\Packages\MyPlugin\
+    sublime.message_dialog("Drink some water")
+    sublime.status_message("Drink some water")
+    
+def write_time():
+    sublime.status_message(time_manage(i))
+
+def time_manage(time_number):
+    time_str='    time:    '+str(int(time_number/60))+'min '+str(time_number%60)+'s'
+    return time_str
 
 class timer(threading.Thread):
     def __init__(self, num, interval):
@@ -20,11 +25,11 @@ class timer(threading.Thread):
     def run(self):
         global i
         while not self.thread_stop:
-            sublime.set_timeout(write_time,1)
+            sublime.set_timeout(write_time, 1)
             i+=1  
-            #if i > 20:
-                #show_notification()
-                #self.thread_stop = True
+            if i % 30 == 0:
+                show_notification()
+                self.pause()
             time.sleep(self.interval)          
     def pause(self):        
         self.thread_stop = True
@@ -33,22 +38,19 @@ class timer(threading.Thread):
         global i
         i=0    
 
-def write_time():
-    sublime.status_message(time_manage(i))
 
-def time_manage(time_number):
-    time_str='    time:    '+str(int(time_number/60))+'min '+str(time_number%60)+'s'
-    return time_str
 
 thread1 = timer(1, 1)
+
+
+
+
 
 class gtimerCommand(sublime_plugin.TextCommand):    
     def run(self, edit):
         global thread1
         thread=timer(1,1) 
-        if not thread1.isAlive():
-            #live=True
-        #else:                               
+        if not thread1.isAlive():                              
             thread.start()
             thread1=thread
 
